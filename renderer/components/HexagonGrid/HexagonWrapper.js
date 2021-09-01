5002405d2f7053052ee223cee9ed9042cfdcc3af
radovancapek <capek.radovan@gmail.com>
2021-09-01 13:21:50 +0200

    styles

--- renderer/components/HexagonGrid/HexagonWrapper.js
+++ renderer/components/HexagonGrid/HexagonWrapper.js
@@ -15,7 +15,6 @@ class HexagonWrapper extends React.Component {
         const fillColor = hexagon.active ? "#00ff00" : "#dddddd";
         return (
             <div className="HexagonWrapper" key={this.key} style={{height: this.props.height, width: this.props.width}}>
-                <div className="center" style={{top: hexagon.center[1] - 1 + "px", left: hexagon.center[0] - 1 + "px"}}></div>
                 <svg>
                     <Polygon
                         points={hexagon.hexaPoints}
